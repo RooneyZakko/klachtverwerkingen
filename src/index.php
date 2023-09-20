d530ac84186873efea6896173cc4f13f9c1c90d8
Rooney <rooney.zakko@gmail.com>
2023-09-20 21:46:23 +0200

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klacht Indienen</title>
+</head>
+<body>
+    <h1>Klacht Indienen</h1>
+    
+    <form method="POST" action="opai.php">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br>
+        
+        <label for="omschrijving">Omschrijving klacht:</label><br>
+        <textarea id="omschrijving" name="omschrijving" rows="5" cols="40" required></textarea><br>
+        
+        <input type="submit" value="Indienen">
+    </form>
+</body>
+</html>
+<?php 
+
+require '../vendor/autoload.php';
+require '../phpmailer/phpmailer';
+
+?> 
---
+++ src/opai.php
@@ -0,0 +1,44 @@
+<?php 
+
+require '../vendor/autoload.php';
+// require '../phpmailer/phpmailer';
+
+
+?> 
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klacht Verwerking</title>
+    <style>
+        *{
+            text-align:center;
+        }
+        h2{
+            color:green;
+        }
+    </style>
+</head>
+<body>
+    
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['name']) && isset($_POST['email'])) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $omschrijving = $_POST['omschrijving'];
+
+            echo "<h2>Uw klacht is in behandeling</h2> ";
+            // echo "<br>";
+            echo "<p><strong>Name:</strong> $name</p>";
+            echo "<p><strong>Email:</strong> $email</p>";;
+        } else {
+            echo "<p>Form data is missing.</p>";
+        }
+    }
+    ?>
+</body>
+</html>
